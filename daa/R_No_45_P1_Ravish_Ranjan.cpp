0797cb42767f38604fb33db5205044c309085ba7
Ravish Ranjan <122588009+Ravish-Ranjan@users.noreply.github.com.>
2026-02-03 15:34:01 +0530

    DAA : restructure

--- daa/R_No_45_P1_Ravish_Ranjan.cpp
+++ daa/R_No_45_P1_Ravish_Ranjan.cpp
@@ -1,34 +1,6 @@
-#include "analyzer.cpp" // analyzer class file import
-
-
-// Inheriting Metric class and overriding algo method
-class InsertionSort : public Metric {
-    public:
-        void algo(json& data,const std::string& key) override{
-            for (int i = 1; i< data[key].size(); i++){
-                json saved_row;
-
-                for (auto [k, v] : data.items()) saved_row[k] = v[i]; // getting key's indexed data for later  
-
-                auto elem = saved_row[key]; // picking field of saved rowto sort the data with 
-                int j = i-1;
-                this->assigns++;
-
-                while (j >= 0){
-                    this->comps++;
-                    if (elem < data[key][j]){
-                        assign(data,j,j+1); // custom assign function for json
-                        this->assigns++;
-                        j--;
-                    } else {
-                        break;
-                    }
-                }
-                assign(data,j+1,saved_row); 
-                this->assigns++;
-            }
-        }
-};
+#include "algos/InsertionSort.cpp"
+#include "util/command.cpp"
+#include "analyzer.cpp"
 
 int main(){
     std::cout << "Installing python dependencies" << std::endl;
