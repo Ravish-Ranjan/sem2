b14b7091fd80f0295d7928804767e0f1528e55e7
Ravish Ranjan <122588009+Ravish-Ranjan@users.noreply.github.com.>
2026-02-11 17:54:02 +0530

    DAA : pract 5 with 2a too

--- daa/R_No_45_P5_Ravish_Ranjan.cpp
+++ daa/R_No_45_P5_Ravish_Ranjan.cpp
@@ -6,6 +6,7 @@ int main(){
     runCommand("pip install -r requirements.txt");
     RandomQuickSort rqs;
 
+    std::cout << "\n\nFirst Questions with Random Quick Sort" << std::endl;
     // first question
     Analyzer analyzer7("float,firstName","age,name",rqs,"RandomQuickSortOnAge");
     analyzer7.analyze(0); // RandomQuick sorting data on age 
@@ -19,17 +20,6 @@ int main(){
     analyzer9.analyze(0); // RandomQuick sorting data first on age
     analyzer9.analyze(1); // RandomQuick sorting data then on name
 
-    Output read("");
-    json r1 = read.getSavedOutput("QuickSortOnAge");
-    json r2 = read.getSavedOutput("QuickSortOnName");
-    json r3 = read.getSavedOutput("QuickSortOnAgeThenName");
-    Graph g1("QuickSortOnAge");
-    Graph g2("QuickSortOnName");
-    Graph g3("QuickSortOnAgeThenName");
-    g1.genGraph(r1["comps"],r1["assigns"]);
-    g2.genGraph(r2["comps"],r2["assigns"]);
-    g3.genGraph(r3["comps"],r3["assigns"]);
-
     json data = json::array({
         json::array({"Reeta",18.5}),
         json::array({"Geeta",17.8}),
@@ -42,6 +32,31 @@ int main(){
     std::cout << std::endl;
     printJson(data);
 
+    std::cout << "\n\nSecond (a) Questions with Random Quick Sort" << std::endl;
+    std::cout << "Sorting(asc) cars based on thier horsepower" << std::endl;
+    std::cout << "Fetching dataset from ucimlrepo dataset-id = 9" << std::endl;
+    runCommand("python ./util/fetchDataset.py id:9 filename:cardata savefiletype:json");
+    std::cout << "dataset fetched in data folder" << std::endl;
+    
+    Analyzer analyzer("","carname,horsepower,weight,cylinders,accelaration",rqs,"CarHorsePowerQuickSorting");
+    analyzer.analyze(2,"util/getcardata.py");
+
+    std::cout << "\n\nResult of First and Second (a) Questions with Quick Sort" << std::endl;
+    Output read("");
+    json r1 = read.getSavedOutput("QuickSortOnAge");
+    json r2 = read.getSavedOutput("QuickSortOnName");
+    json r3 = read.getSavedOutput("QuickSortOnAgeThenName");
+    json r4 = read.getSavedOutput("CarHorsePowerQuickSorting");
+    Graph g1("QuickSortOnAge");
+    Graph g2("QuickSortOnName");
+    Graph g3("QuickSortOnAgeThenName");
+    Graph g4("CarHorsePowerQuickSorting");
+    g1.genGraph(r1["comps"],r1["assigns"]);
+    g2.genGraph(r2["comps"],r2["assigns"]);
+    g3.genGraph(r3["comps"],r3["assigns"]);
+    g4.genGraph(r4["comps"],r4["assigns"]);
+
+    std::cout << "\n\nSecond (b) Questions with Random Quick Sort" << std::endl;
     std::cout << "Sorting(asc) dry bean data based on thier perimeter" << std::endl;
     std::cout << "Fetching dataset from ucimlrepo dataset-id = 602" << std::endl;
     runCommand("python ./util/fetchDataset.py id:602 filename:drybean savefiletype:json");
