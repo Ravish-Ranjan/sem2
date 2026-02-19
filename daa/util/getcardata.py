c20932d894b09017a9d136350a9c23849dca25b6
Ravish Ranjan <122588009+Ravish-Ranjan@users.noreply.github.com.>
2026-02-19 08:28:14 +0530

    DAA : input base generation

--- daa/util/getcardata.py
+++ daa/util/getcardata.py
@@ -8,6 +8,7 @@ if (len(sys.argv) < 2):
 
 size = 10
 fileName = "input-data"
+refresh = False
 
 for i in range(1,len(sys.argv)):
     label,value = sys.argv[i].split(":")
@@ -15,8 +16,14 @@ for i in range(1,len(sys.argv)):
         size = int(value)
     elif (label == "filename"):
         fileName = value
+    elif (label == "refresh"):
+        refresh = True
 
 allCarData = []
+
+if (not refresh and Path(f"input/{fileName}-{size}-size.json").exists()):
+    print(f"./input/{fileName}-{size}-size.json")
+    exit(0)
 
 with open("data/cardata.json","r") as dataset:
     allCarData = json.load(dataset)
