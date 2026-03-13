86ddfed01414aeb2d625c0828625ee45af829885
Ravish Ranjan <122588009+Ravish-Ranjan@users.noreply.github.com.>
2026-03-13 12:09:03 +0530

    DAA : pract 6

--- daa/R_No_45_P6_Ravish_Ranjan.cpp
+++ daa/R_No_45_P6_Ravish_Ranjan.cpp
@@ -1,6 +1,6 @@
 #include "analyzer.cpp"
-#include "algos/RadixSort.cpp"
 #include "graph.cpp"
+#include "algos/RadixSort.cpp"
 
 int main(){
     std::cout << "Installing python dependencies" << std::endl;
@@ -9,90 +9,91 @@ int main(){
 
     std::cout << "\n\nFirst Questions with Radix Sort" << std::endl;
     // first question
-    Analyzer analyzer7("int,firstName","age,name",rxs,"RadixSortOnAge",false,true);
-    analyzer7.analyze(0,"RadixSortOnAge","util/genradixdata.py"); // Radix sorting data on age 
+    Analyzer analyzer1("int,firstName","age,name",rxs,"RadixSortOnAge",true);
+    json ana1 = analyzer1.analyze(0,"RadixSortOnAge","util/genradixdata.py"); // Radix sorting data on age 
     
     // second question
-    Analyzer analyzer8("int,firstName","age,name",rxs,"RadixSortOnName",false,true);
-    analyzer8.analyze(1,"RadixSortOnName","util/genradixdata.py"); // Radix sorting data on name
+    Analyzer analyzer2("int,firstName","age,name",rxs,"RadixSortOnName",true);
+    json ana2 = analyzer2.analyze(1,"RadixSortOnName","util/genradixdata.py"); // Radix sorting data on name
     
     // third question (with persmstent data)
-    Analyzer analyzer9("int,firstName","age,name",rxs,"RadixSortOnAgeThenName",true,true);
-    analyzer9.analyze(0,"RadixSortOnAgeThenName","util/genradixdata.py"); // Radix sorting data first on age
-    analyzer9.analyze(1,"RadixSortOnAgeThenName","util/genradixdata.py"); // Radix sorting data then on name
+    Analyzer analyzer3("int,firstName","age,name",rxs,"RadixSortOnAgeThenName",true,true);
+    analyzer3.analyze(0,"RadixSortOnAgeThenName","util/genradixdata.py"); // Radix sorting data first on age
+    json ana3 = analyzer3.analyze(1,"RadixSortOnAgeThenName","util/genradixdata.py"); // Radix sorting data then on name
 
-    json data = json::array({
-        json::array({"Reeta",18}),
-        json::array({"Geeta",17}),
-        json::array({"Reeta",18})
-    });
-    std::cout << "Original Data (Radix Sort)" << std::endl;
-    printJson(data);
-    rxs.algo(data,0);
-    rxs.algo(data,1);
-    std::cout << std::endl;
-    printJson(data);
 
-    std::cout << "\n\nSecond (a) Questions with Radix Sort" << std::endl;
-    std::cout << "Sorting(asc) cars based on thier horsepower" << std::endl;
-    std::cout << "Fetching dataset from ucimlrepo dataset-id = 9" << std::endl;
-    runCommand("python ./util/fetchDataset.py id:9 filename:cardata savefiletype:json");
-    std::cout << "dataset fetched in data folder" << std::endl;
+    std::cout << "\n\nResult of First Questions with all previous" << std::endl;
+    Output read("");
+    json r1 = read.getSavedOutput("InsertionSortOnAge");
+    json r2 = read.getSavedOutput("InsertionSortOnName");
+    json r3 = read.getSavedOutput("InsertionSortOnAgeThenName");
     
-    Analyzer analyzer("","carname,horsepower,weight,cylinders,accelaration",rxs,"CarHorsePowerSorting",false,true);
-    analyzer.analyze(1,"CarHorsePowerRadixSorting","util/getcardata.py");
-
-    // std::cout << "\n\nResult of First and Second (a) Questions with Quick Sort" << std::endl;
-    // Output read("");
-    // json r1 = read.getSavedOutput("QuickSortOnAge");
-    // json r2 = read.getSavedOutput("QuickSortOnName");
-    // json r3 = read.getSavedOutput("QuickSortOnAgeThenName");
-    // json r4 = read.getSavedOutput("CarHorsePowerQuickSorting");
-    // Graph g("QuickSortOnAge");
-    // g.genGraph({r1["comps"],r1["assigns"]},{"comparisions","assignments"});
+    json r4 = read.getSavedOutput("MergeSortOnAge");
+    json r5 = read.getSavedOutput("MergeSortOnName");
+    json r6 = read.getSavedOutput("MergeSortOnAgeThenName");
     
-    // g.graphName = "QuickSortOnName";
-    // g.genGraph({r2["comps"],r2["assigns"]},{"comparisions","assignments"});
+    json r7 = read.getSavedOutput("QuickSortOnAge");
+    json r8 = read.getSavedOutput("QuickSortOnName");
+    json r9 = read.getSavedOutput("QuickSortOnAgeThenName");
     
-    // g.graphName = "QuickSortOnAgeThenName";
-    // g.genGraph({r3["comps"],r3["assigns"]},{"comparisions","assignments"});
+    json r10 = read.getSavedOutput("RandomQuickSortOnAge");
+    json r11 = read.getSavedOutput("RandomQuickSortOnName");
+    json r12 = read.getSavedOutput("RandomQuickSortOnAgeThenName");
     
-    // g.graphName = "CarHorsePowerQuickSorting";
-    // g.genGraph({r4["comps"],r4["assigns"]},{"comparisions","assignments"});
+    Graph g("p5_random_quick_sort_assignments");
+ 
+    g.genGraph(
+        {
+            ana1["assigns"],ana2["assigns"],ana3["assigns"],
+            r1["assigns"],r2["assigns"],r3["assigns"],
+            r4["assigns"],r5["assigns"],r6["assigns"],
+            r7["assigns"],r8["assigns"],r9["assigns"],
+            r10["assigns"],r11["assigns"],r12["assigns"]
+        },
+        {
+            "on_age","on_name","on_age_then_name",
+            "ins_on_age","ins_on_name","ins_on_age_then_name",
+            "mrg_on_age","mrg_on_name","mrg_on_age_then_name",
+            "quic_on_age","quic_on_name","quic_on_age_then_name",
+            "rand_quic_on_age","rand_quic_on_name","rand_quic_on_age_then_name",
+        }
+    );
 
-    std::cout << "\n\nSecond (b) Questions with Radix Sort" << std::endl;
-    std::cout << "Sorting(asc) dry bean data based on thier perimeter" << std::endl;
-    std::cout << "Fetching dataset from ucimlrepo dataset-id = 602" << std::endl;
-    runCommand("python ./util/fetchDataset.py id:602 filename:drybean savefiletype:json");
+    g.graphName = "p5_random_quick_sort_comparisions";
+    g.genGraph(
+        {
+            ana1["comps"],ana2["comps"],ana3["comps"],
+            r1["comps"],r2["comps"],r3["comps"],
+            r4["comps"],r5["comps"],r6["comps"],
+            r7["comps"],r8["comps"],r9["comps"],
+            r10["comps"],r11["comps"],r12["comps"]
+        },
+        {
+            "on_age","on_name","on_age_then_name",
+            "ins_on_age","ins_on_name","ins_on_age_then_name",
+            "mrg_on_age","mrg_on_name","mrg_on_age_then_name",
+            "quic_on_age","quic_on_name","quic_on_age_then_name",
+            "rand_quic_on_age","rand_quic_on_name","rand_quic_on_age_then_name",
+        }
+    );
     
-    std::unordered_map<std::string,int> columnMap;
-    columnMap["Area"] = 0;
-    columnMap["Perimeter"] = 1;
-    columnMap["MajorAxisLength"] = 2;
-    columnMap["MinorAxisLength"] = 3;
-    columnMap["AspectRatio"] = 4;
-
-
-    std::cout << "dataset fetched in data folder" << std::endl;
-    std::cout << "estimated time of completion is around 3.5 mins" << std::endl;
-
-    Input inp;
-    inp.inputFile = "./data/drybean.json";
-    inp.getInput();
-
-    rxs.algo(inp.input,columnMap["Perimeter"]);
-    std::cout << "\nComparisions (Radix Sort): " << rxs.comps << std::endl;
-    std::cout << "Assignments (Radix Sort): " << rxs.assigns << std::endl;
-
-    // Output readOld("");
-    // json isData = readOld.getSavedOutput("DryBeanDataQuickSort");
-    // std::cout << "Comparisions (Quick Sort): " << isData["comps"][0] << std::endl;
-    // std::cout << "Assignments (Quick Sort): " << isData["assigns"][0] << std::endl;
-
-    Output out("DryBeanDataRadixSort");
-    out.outputs["data"] = inp.input;
-    out.outputs["comps"] = {rxs.comps};
-    out.outputs["assigns"] = {rxs.assigns};
-    out.saveOutput();
+    g.graphName = "p5_random_quick_sort_time_microsec";
+    g.genGraph(
+        {
+            ana1["time"],ana2["time"],ana3["time"],
+            r1["c-time"],r2["c-time"],r3["c-time"],
+            r4["c-time"],r5["c-time"],r6["c-time"],
+            r7["c-time"],r8["c-time"],r9["c-time"],
+            r10["c-time"],r11["c-time"],r12["c-time"]
+        },
+        {
+            "on_age","on_name","on_age_then_name",
+            "ins_on_age","ins_on_name","ins_on_age_then_name",
+            "mrg_on_age","mrg_on_name","mrg_on_age_then_name",
+            "quic_on_age","quic_on_name","quic_on_age_then_name",
+            "rand_quic_on_age","rand_quic_on_name","rand_quic_on_age_then_name",
+        }
+    );
+    
     return 0;
 }
